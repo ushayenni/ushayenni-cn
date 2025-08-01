2ec9fb5082e610fbb83fec03f566f2218e942f68
ushayenni <151708074+ushayenni@users.noreply.github.com>
2025-08-01 12:11:55 +0530

    Create checksum.c

---
+++ checksum.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+ int sender(int arr[10],int n)
+{
+    int checksum,sum=0,i;
+printf("\n****SENDER SIDE****\n");
+    for(i=0;i<n;i++)
+    sum+=arr[i];
+printf("SUM IS: %d",sum);
+    checksum=~sum;    //1's complement of sum
+printf("\nCHECKSUMIS:%d",checksum);
+    return checksum;
+}
+void receiver(int arr[10],intn,int sch)
+{
+    int checksum,sum=0,i;
+printf("\n\n****RECEIVER SIDE****\n");
+    for(i=0;i<n;i++)
+        sum+=arr[i];
+printf("SUM IS:%d",sum);
+    sum=sum+sch;
+    checksum=~sum;    //1's complement of sum
+printf("\nCHECKSUMIS:%d",checksum);
+}
+void main()
+{
+    int n,sch,rch;
+printf("\nENTER SIZE OF THE STRING:");
+scanf("%d",&n);
+    int arr[n];
+printf("ENTER THE ELEMENTS OF THE ARRAY TO CALCULATE CHECKSUM:\n");
+for(int i=0;i<n;i++)
+    {
+scanf("%d",&arr[i]);
+    }
+    sch=sender(arr,n);
+    receiver(arr,n,sch);
+}
